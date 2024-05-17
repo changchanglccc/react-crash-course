563bf2739fd014529b9c7e87f003f515065d30a4
changchanglccc <jennifer06091992@gmail.com>
2024-05-17 13:18:42 -0400

    feat: made Cancel button works

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,34 +1,19 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
-import { useState } from 'react';
 import Modal from './Modal';
 
 function PostsList({isPosting, onStopPosting}) {
-
-    const [enterBody, setEnterBody] = useState('');
-    const [enterAuthor, setEnterAuthor] = useState('');
-
-    function changeBodyHandler(event) {
-        setEnterBody(event.target.value);
-    }
-
-    function changeAuthorHandler(event) {
-        setEnterAuthor(event.target.value);
-    }
 
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
-                    <NewPost
-                        onBodyChange={changeBodyHandler}
-                        onAuthorChange={changeAuthorHandler}/>
+                    <NewPost onCancel={onStopPosting}/>
                 </Modal>
             )}
 
             <ul className={classes.posts}>
-                <Post author={enterAuthor} body={enterBody}/>
                 <Post author="Jenny" body="check here!"/>
                 <Post author="James" body="Hello, my name is James!"/>
             </ul>
