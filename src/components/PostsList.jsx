2df189910fc9d861640ab0afbf4bb7c6ef0961b7
changchanglccc <jennifer06091992@gmail.com>
2024-05-16 21:28:50 -0400

    feat: added NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Jennifer" body="React is awesome!"/>
-            <Post author="Jenny" body="check here!"/>
-            <Post author="James" body="Hello, my name is James!"/>
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Jennifer" body="React is awesome!"/>
+                <Post author="Jenny" body="check here!"/>
+                <Post author="James" body="Hello, my name is James!"/>
+            </ul>
+        </>
     );
 }
 
