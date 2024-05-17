b6676e2aba6173c3c327a976c2179f146c3163eb
changchanglccc <jennifer06091992@gmail.com>
2024-05-17 00:05:51 -0400

    chore: lifted state up
    - NewPost component is to deal the state, use "props" to set two props for NewPost
    - PostsList component: use useState and changeXXXHandler set eventlistener
    - can set multiple state for a component
    - lift state from NewPost itself to PostsList(include NewPost invoke)

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,26 @@
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
+import { useState } from 'react';
 
 function PostsList() {
+    const [enterBody, setEnterBody] = useState('');
+    const [enterName, setEnterName] = useState('');
+
+    function changeBodyHandler(event) {
+        setEnterBody(event.target.value);
+    }
+
+    function changeNameHandler(event) {
+        setEnterName(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onNameChange={changeNameHandler}/>
+
             <ul className={classes.posts}>
-                <Post author="Jennifer" body="React is awesome!"/>
+                <Post author={enterName} body={enterBody}/>
                 <Post author="Jenny" body="check here!"/>
                 <Post author="James" body="Hello, my name is James!"/>
             </ul>
