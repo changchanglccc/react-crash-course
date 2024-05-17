fd12ef55e82ef8d39b0c5fd0b1632634898dda0f
changchanglccc <jennifer06091992@gmail.com>
2024-05-16 20:44:13 -0400

    chore: added CSS module

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Post from './components/Post';
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Jennifer" body="React is awesome!"/>
+      <Post author="Jenny" body="check here!"/>
+      <Post author="James" body="Hello, my name is James!"/>
     </main>
   );
 }
